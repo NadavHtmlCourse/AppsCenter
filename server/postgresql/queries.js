039699d83fe880d3137271584425939763c98f1a
07ItamarMelamed <nadav8.tch07@gmail.com>
2022-08-24 15:12:11 +0300

    Message

--- server/postgresql/queries.js
+++ server/postgresql/queries.js
@@ -106,7 +106,7 @@ const updateUser = (request, response) => {
     const { name, email } = request.body;
 
     pool.query(
-        'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+        'UPDATE users SET name=$1, email=$2 WHERE id=$3',
         [name, email, id],
         (error, results) => {
             if (error) {
